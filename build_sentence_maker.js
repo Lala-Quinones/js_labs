b54b9240648fc3b18ff8991e735e07e29fde27ed
Lala Quinones <lala.justnorth@gmail.com>
2025-04-19 12:45:01 -0700

    Create build_sentence_maker.js

---
+++ build_sentence_maker.js
@@ -0,0 +1,18 @@
+let adjective = "spoiled";
+let noun = "alligator";
+let verb = "slepping";
+let place = "castle";
+let adjective2 = "astounding";
+let noun2 = "horses";
+
+let firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+console.log("First story: " + firstStory);
+
+adjective = "smelly";
+noun = "cat";
+verb = "meowing";
+place = "forest";
+adjective2 = "tiny";
+noun2 = "fish";
+let secondStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+console.log("Second story: " + secondStory);
