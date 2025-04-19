5e06dfbf78e7aacb4f0b04b13e6f91286a728319
Lala Quinones <lala.justnorth@gmail.com>
2025-04-19 12:25:09 -0700

    Add files via upload

---
+++ lab-javascript-trivia-bot.js
@@ -0,0 +1,28 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+let botName;
+let botLocation;
+let favoriteLanguage;
+let introSentence;
+
+botName = "Alexander";
+botLocation = "Alexandria";
+favoriteLanguage = "JavaScript";
+
+introSentence = "My name is " + botName + " and I live on " + botLocation + ".";
+console.log(introSentence);
+
+let favoriteLanguageSentence;
+favoriteLanguageSentence = "My favorite programming language is " + favoriteLanguage + ".";
+console.log(favoriteLanguageSentence);
+
+let codingFact = favoriteLanguage + ", is widely known and was created in just 10 days!";
+console.log(codingFact);
+
+codingFact = "Also, while " + favoriteLanguage + " and Java have similar names, they're very different languages.";
+console.log(codingFact);
+
+codingFact = "One more fun fact about " + favoriteLanguage + " is that it is used in both front-end and back-end development!";
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".");
