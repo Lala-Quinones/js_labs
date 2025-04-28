7c13c4eb9cebdeca91d018f3615fdad0058fe094
Lala Quinones <lala.justnorth@gmail.com>
2025-04-28 00:10:17 -0700

    Add files via upload

---
+++ lab-build-a-factorial-calculator.js
@@ -0,0 +1,13 @@
+const num = 15;
+function factorialCalculator(x) {
+  let result = 1;
+  for (let i = x; i > 0; i--) {
+    result *= i;
+  }
+  return result
+}
+
+const factorial = factorialCalculator(num);
+
+const resultMsg = `Factorial of ${num} is ${factorial}`
+console.log(resultMsg);
