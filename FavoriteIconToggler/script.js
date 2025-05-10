f3a34d70abb8b66c014e4747cc3c2ed322327e3f
Lala Quinones <lala.justnorth@gmail.com>
2025-05-09 17:18:01 -0700

    Add files via upload

---
+++ FavoriteIconToggler/script.js
@@ -0,0 +1,10 @@
+document.querySelectorAll('.favorite-icon').forEach(button => {
+    button.addEventListener('click', () => {
+        button.classList.toggle('filled');
+        if(button.classList.contains('filled')) {
+            button.innerHTML = '&#10084';
+        } else {
+            button.innerHTML = '&#9825';
+        }
+    });
+});
